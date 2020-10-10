339137fabcd9cc175e59c5ccdd69fc3d96def560
Teckeyarul <40233956+arul236@users.noreply.github.com>
2020-10-10 10:57:31 +0530

    Add files via upload

---
+++ engish and tami cgpa program.py
@@ -0,0 +1,8 @@
+x: float(input("enter tamil\english 1 mark:"))
+y= float(input("enter tamil\english 2 mark:"))
+z= float(input("enter tamil\english 3 mark:"))
+a= float(input("enter tamil\english 4 mark:"))
+b= (3*x)+(3*y)+(3*z)+(3*a)
+c=b/12
+print("tamil\english language CGPA is :",c)
+
